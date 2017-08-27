d68bbdbe32125bfd16829b6e60b1b3f737b065ec
shenmo <626673166@qq.com>
2017-08-27 21:15:50 +0800

    mineui
    2017/8/27 21

---
+++ js/mineui-test1.js
@@ -0,0 +1,22 @@
+$(function() {
+  init();
+
+  $('.mineui-input-radio').click(function() {
+    $(this).parent().find('.mineui-input-radio').removeClass('mineui-input-radio-checked').prev();
+    $(this).parent().find('.mineui-input-radio > i').removeClass('mineui-anim-scaleSpring');
+    $('input[type="radio"]').parent().find('input[type="radio"]').removeAttr('checked');
+
+    $(this).addClass('mineui-input-radio-checked');
+    $(this).find('i').addClass('mineui-anim-scaleSpring');
+    $(this).prev().attr('checked','').click();
+  });
+});
+
+function init() {
+  var radios = $('input[type="radio"]');
+  var templet = '<div class="mineui-input-radio"><i></i><span>[input.value]</span></div>';
+  for (var i = 0; i < radios.length; i++) {
+    var radio = templet.replace('[input.value]', radios.get(i).value);
+    $(radios.get(i)).after(radio);
+  }
+}
